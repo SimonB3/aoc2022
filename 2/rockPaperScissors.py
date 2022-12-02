4594ff674273035cc0e35d897689630ce8a368d0
Simon Molander <simon.molander@b3.se>
2022-12-02 11:35:45 +0100

    Remove hardcoded conversion tables

--- 2/rockPaperScissors.py
+++ 2/rockPaperScissors.py
@@ -1,45 +1,39 @@
 import sys
 
 class Match:
-    winScoreDict = {
-        ('A', 'A'): 3,
-        ('A', 'B'): 6,
-        ('A', 'C'): 0,
-        ('B', 'A'): 0,
-        ('B', 'B'): 3,
-        ('B', 'C'): 6,
-        ('C', 'A'): 6,
-        ('C', 'B'): 0,
-        ('C', 'C'): 3
-    }
-
     choiceScoreDict = {
         'A': 1,
         'B': 2,
         'C': 3
     }
 
+    outcomeDict = {
+        'X': 2,
+        'Y': 0,
+        'Z': 1
+    }
+
     ownChoiceDict = {
-        ('A', 'X'): 'C',
-        ('A', 'Y'): 'A',
-        ('A', 'Z'): 'B',
-        ('B', 'X'): 'A',
-        ('B', 'Y'): 'B',
-        ('B', 'Z'): 'C',
-        ('C', 'X'): 'B',
-        ('C', 'Y'): 'C',
-        ('C', 'Z'): 'A',
+        0: 'C',
+        1: 'A',
+        2: 'B',
     }
 
     def __init__(self, opponent, outcome) -> None:
         self.opponent = opponent
-        self.me = self.ownChoiceDict[opponent, outcome]
+        self.me = self.getOwnChoice(opponent, outcome)
 
     def __str__(self) -> str:
-        return f"{self.opponent}, {self.me}, Score: {self.getTotalScore()}"
+        return f"{self.opponent}, {self.me}, Score: {self.calculateWinScore()} + {self.choiceScoreDict[self.me]} = {self.getTotalScore()}"
+
+    def getOwnChoice(self, opponent, outcome):
+        return self.ownChoiceDict[(self.choiceScoreDict[opponent] + self.outcomeDict[outcome]) % 3]
+
+    def calculateWinScore(self):
+        return ((((self.choiceScoreDict[self.opponent] - self.choiceScoreDict[self.me]) *-1) + 4) % 3) * 3
 
     def getTotalScore(self):
-        return self.winScoreDict[(self.opponent, self.me)] + self.choiceScoreDict[self.me]
+        return self.calculateWinScore() + self.choiceScoreDict[self.me]
 
 def loadMatchHistory(filename):
     matchHistory = []
@@ -59,4 +53,6 @@ def getFileName():
 matchHistory = loadMatchHistory(getFileName())
 matchHistoryScores = [match.getTotalScore() for match in matchHistory]
 
+# [print(match) for match in matchHistory]
+# print(matchHistoryScores)
 print(sum(matchHistoryScores))
