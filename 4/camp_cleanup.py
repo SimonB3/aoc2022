5aa9057e25ef2316ed7f0d8b9683fc708ba01acc
Simon Molander <simon.molander@b3.se>
2022-12-04 19:57:57 +0100

    Completed 4:1

---
+++ 4/camp_cleanup.py
@@ -0,0 +1,23 @@
+import sys
+
+def loadSectionAssignmentSetPairs(filename):
+    allRangePairs = []
+    with open(filename) as file:
+        for line in file.readlines():
+            sections_starts_and_stops = [[int(rawval) for rawval in section.split('-')] for section in line.strip().split(',')]
+            allRangePairs.append([set(range(innerlist[0], innerlist[1]+1)) for innerlist in sections_starts_and_stops])            
+
+    return allRangePairs
+
+def getFileName():
+    if (len(sys.argv) < 2):
+        filename = 'input'
+    else:
+        filename = sys.argv[1]
+    return filename
+
+allSectionPairs = loadSectionAssignmentSetPairs(getFileName())
+#print(allSectionPairs)
+isSectionContained = [sections[0].issubset(sections[1]) or sections[1].issubset(sections[0]) for sections in allSectionPairs]
+#print(isSectionContained)
+print(sum(isSectionContained))
