2a810ed3636565290780c02dec816cc235706f0c
Simon Molander <simon.molander@b3.se>
2022-12-04 20:36:23 +0100

    Completed 3:2

--- 3/rucksacks.py
+++ 3/rucksacks.py
@@ -16,13 +16,27 @@ def getFileName():
         filename = sys.argv[1]
     return filename
 
-def itemValue(item):
+def itemPriority(item):
     if item.islower():
         return ord(item) - ord('a') + 1
     else:
         return ord(item) - ord('A') + 27
 
+def uncompartmentalize(rucksacksWithCompartments):
+    return [compartments[0].union(compartments[1]) for compartments in allRucksacks]
+
+def makeGroupsOfThree(fullRucksacks):
+    groupsOfThree = []
+    for index in range(0, len(fullRucksacks)-2, 3):
+        groupsOfThree.append([fullRucksacks[index], fullRucksacks[index+1], fullRucksacks[index+2]])
+
+    return groupsOfThree
+
 allRucksacks = loadRucksacks(getFileName())
 commonItems = [compartments[0].intersection(compartments[1]) for compartments in allRucksacks]
-commonItemsPriority = [itemValue(item.pop()) for item in commonItems]
+commonItemsPriority = [itemPriority(item.pop()) for item in commonItems]
 print(sum(commonItemsPriority))
+groupedRucksacks = makeGroupsOfThree(uncompartmentalize(allRucksacks))
+commonItemsInGroup = [rucksackGroup[0].intersection(rucksackGroup[1]).intersection(rucksackGroup[2]) for rucksackGroup in groupedRucksacks]
+commonItemsInGroupPriority = [itemPriority(item.pop()) for item in commonItemsInGroup]
+print(sum(commonItemsInGroupPriority))
