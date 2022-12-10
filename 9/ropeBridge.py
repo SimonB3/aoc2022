460e65a7ad0983347dc2cb6487af2ebdb6acaf1d
Simon Molander <simon.molander@b3.se>
2022-12-10 19:04:17 +0100

    Completed 9:2

--- 9/ropeBridge.py
+++ 9/ropeBridge.py
@@ -18,12 +18,7 @@ def getFileName():
         filename = sys.argv[1]
     return filename
 
-headPos = np.array([0, 0])
-tailPos = np.array([0, 0])
-tailVisitedPosition = set()
-for move in loadMovelist(getFileName()):
-    headPos+=move
-    
+def calculateNewTailPos(headPos, tailPos):
     tailHeadDistance=headPos-tailPos
     if (abs(tailHeadDistance[0]) > 1):
         tailPos[0]+=np.sign(tailHeadDistance[0])
@@ -33,8 +28,36 @@ for move in loadMovelist(getFileName()):
         tailPos[1]+=np.sign(tailHeadDistance[1])
         if(abs(tailHeadDistance[0]) > 0):
             tailPos[0]+=np.sign(tailHeadDistance[0])
+
+  #  print(f"head: {headPos}, tail: {tailPos}, distance (before tail move): {tailHeadDistance}, distance (after tail move): {headPos-tailPos}")
+
+    return tailPos
+
+headPos = np.array([0, 0])
+knotPos1 = np.array([0, 0])
+knotPos2 = np.array([0, 0])
+knotPos3 = np.array([0, 0])
+knotPos4 = np.array([0, 0])
+knotPos5 = np.array([0, 0])
+knotPos6 = np.array([0, 0])
+knotPos7 = np.array([0, 0])
+knotPos8 = np.array([0, 0])
+tailPos = np.array([0, 0])
+tailVisitedPosition = set()
+for move in loadMovelist(getFileName()):
+    headPos+=move
+    
+    knotPos1 = calculateNewTailPos(headPos, knotPos1)
+    knotPos2 = calculateNewTailPos(knotPos1, knotPos2)
+    knotPos3 = calculateNewTailPos(knotPos2, knotPos3)
+    knotPos4 = calculateNewTailPos(knotPos3, knotPos4)
+    knotPos5 = calculateNewTailPos(knotPos4, knotPos5)
+    knotPos6 = calculateNewTailPos(knotPos5, knotPos6)
+    knotPos7 = calculateNewTailPos(knotPos6, knotPos7)
+    knotPos8 = calculateNewTailPos(knotPos7, knotPos8)
+    tailPos = calculateNewTailPos(knotPos8, tailPos)
     tailVisitedPosition.add((tailPos[0], tailPos[1]))
 
-    # print(f"head: {headPos}, tail: {tailPos}, distance (before tail move): {tailHeadDistance}, distance (after tail move): {headPos-tailPos}")
+    
 
 print(f"tail visited {len(tailVisitedPosition)} positions")
